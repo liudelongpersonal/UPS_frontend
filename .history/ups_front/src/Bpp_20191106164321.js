ca43eb2482cfbf8119933efac547c369389cc807
Delong Liu <>
2019-11-06 16:50:01 +0800

    start

---
+++ .history/ups_front/src/Bpp_20191106164321.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Bpp extends Component{
+    render() {
+        return (
+          <h1>im BBB</h1>
+        );
+    }
+}
+
+
+
+export default Bpp;
---
+++ .history/ups_front/src/Nav_20191106164809.js
@@ -0,0 +1,3 @@
+import React from 'React';
+import {NavLink} from 'react-router-dom';
+
